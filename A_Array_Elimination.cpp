a0441b715607b026bb2b1eda4f7673b562c48aa4
Sarafaraj Nasardi <sarafarajnasardi786@gmail.com>
2025-06-04 19:25:11 +0530

    hello

--- A_Array_Elimination.cpp
+++ A_Array_Elimination.cpp
@@ -1,135 +1,167 @@
-
-// Author : Sarafaraj Nasardi
-
-
-#pragma GCC optimize("Ofast")
-#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,fma")
-#pragma GCC optimize("unroll-loops")
 #include <bits/stdc++.h>
-#include <complex>
-#include <queue>
-#include <set>
-#include <unordered_set>
-#include <list>
-#include <chrono>
-#include <random>
-#include <iostream>
-#include <algorithm>
-#include <cmath>
-#include <string>
-#include <vector>
-#include <map>
-#include <unordered_map>
-#include <stack>
-#include <iomanip>
-#include <fstream>
-
 using namespace std;
+using ll = long long;
 
-typedef long long ll;
-typedef long double ld;
-typedef pair<int,int> pii;
-typedef pair<ll,ll> pll;
-typedef pair<double,double> pdd;
-typedef vector<ll> vll;
-typedef vector<int> vi;
-typedef vector<vector<int>> vvi;
-typedef vector<vector<ll>> vvll;
-typedef vector<vector<pll>> vvpll;
-typedef vector<pll> vpll;
-typedef vector<pii> vpii;
-typedef map<int, int> mii;
-typedef map<ll,ll> mll;
+struct SegTree {
+    int N;
+    vector<ll> st;
+    SegTree(int n): N(n), st(4*n,0) {}
 
-const ll MOD = 998244353;
-const double eps = 1e-12;
-
-#define forn(i,e) for(ll i = 0; i < e; i++)
-#define forsn(i,s,e) for(ll i = s; i < e; i++)
-#define rforn(i,s) for(ll i = s; i >= 0; i--)
-#define rforsn(i,s,e) for(ll i = s; i >= e; i--)
-#define ln "\n"
-#define dbg(x) cout<<#x<<" = "<<x<<ln
-#define mp make_pair
-#define pb push_back
-#define fi first
-#define se second
-#define INF 2e18
-#define fast_cin() ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
-#define all(x) (x).begin(), (x).end()
-#define sz(x) ((ll)(x).size())
-#define yes cout << "YES\n" ;
-#define no cout << "NO\n" ;
-#define iv(v,n) vector<ll> v(n); forn(i,n) cin >> v[i] ;
-
-
-// unique sort :
-// sort(all(v));
-// v.resize(unique(all(v)) - v.begin()) ;
-
-// Mathematical functions
-ll gcd(ll a, ll b){if (b == 0)return a;return gcd(b, a % b);} //__gcd 
-
-
-ll lcm(ll a, ll b){return (a/gcd(a,b)*b);}
-
-ll moduloMultiplication(ll a,ll b,ll mod){ll res = 0;a %= mod;while (b){if (b & 1)res = (res + a) % mod;b >>= 1;}return res;}
-
-ll powermod( ll x, ll y, ll mod){ ll ans = 1;if( y == 0) return 1;while(y){   if(y%2) ans = (1ll*ans*x)%mod;    x = (1ll*x*x)%mod;    y /= 2;}return ans;}
-
-ll binexp(ll a , ll b , ll m){
-   ll result = 1;
-   while(b > 0){
-      if(b & 1){
-        result = ((result % m) * (a % m)) % m;
-  }
-     a = ((a % m) * (a % m)) % m;
-b >>= 1;
-}
-return result;
-}
-
-vector<vector<long long>> matrixmultiply(const vector<vector<long long>>& matrix1, const vector<vector<long long>>& matrix2) {
-    int rows = matrix1.size();
-    int cols = matrix2[0].size();
-    int common = matrix2.size();
-    vector<vector<long long>> result(rows, vector<long long>(cols, 0));
-
-    for (int i = 0; i < rows; i++) {
-        for (int j = 0; j < cols; j++) {
-            for (int k = 0; k < common; k++) {
-                result[i][j] += matrix1[i][k] * matrix2[k][j];
-            }
+    void build(int p, int l, int r, const vector<ll>& base) {
+        if(l==r) {
+            st[p]=base[l];
+        } else {
+            int m=(l+r)>>1;
+            build(p<<1,l,m,base);
+            build(p<<1|1,m+1,r,base);
+            st[p]=st[p<<1]+st[p<<1|1];
         }
     }
-    return result;
-}
-
-
-
-//Factorial by vector
-//vll fact;
-//void factorials(ll mod, ll maxi){fact.resize(maxi + 1, 1); for(int i = 2; i<= maxi ; i++) fact[i] = (fact[i-1]*i)%mod ;}
-//ll nCr(ll n, ll r, ll mod){ if(r > n) return 0; ll x = (fact[r]*fact[n-r])%mod ;ll ans = (fact[n] * (powermod(x,mod-2, mod)))%mod;return ans;}
-bool isPrime(ll n){if(n<=1)return false;if(n<=3)return true;if(n%2==0||n%3==0)return false;for(int i=5;i*i<=n;i=i+6)if(n%i==0||n%(i+2)==0)return false;return true;}
-
-bool isPowerOfTwo(int n){if(n==0)return false;return (ceil(log2(n)) == floor(log2(n)));}
-
-bool isPerfectSquare(ll x){if (x >= 0) {ll sr = sqrt(x);return (sr * sr == x);}return false;}
-
-
-
-void solve() {
-    ll a,b;
-    cin>>a>>b;
-}
-
-int main() {
-    fast_cin();
-    ll t;
-    cin >> t;
-    for(int it=1;it<=t;it++) {
-        solve();
+    ll query(int p,int l,int r,int i,int j){
+        if(j<l||r<i) return 0;
+        if(i<=l&&r<=j) return st[p];
+        int m=(l+r)>>1;
+        return query(p<<1,l,m,i,j)
+             + query(p<<1|1,m+1,r,i,j);
     }
+    void update(int p,int l,int r,int idx,ll val){
+        if(l==r){
+            st[p]=val;
+        } else {
+            int m=(l+r)>>1;
+            if(idx<=m) update(p<<1,l,m,idx,val);
+            else        update(p<<1|1,m+1,r,idx,val);
+            st[p]=st[p<<1]+st[p<<1|1];
+        }
+    }
+};
+
+int N, R;
+vector<vector<pair<int,int>>> adj; 
+// adj[u]: list of (v, edge_id)
+
+vector<int> parent, depth, heavy, sz;
+vector<ll> edge_w;       // edge_w[eid] = weight of that edge
+vector<int> parent_eid;  // parent_eid[u] = id of edge (parent[u],u)
+
+int dfs(int u){
+    sz[u]=1;
+    int maxSub=0;
+    for(auto [v,eid]: adj[u]){
+        if(v==parent[u]) continue;
+        parent[v]=u;
+        parent_eid[v]=eid;
+        depth[v]=depth[u]+1;
+        int s = dfs(v);
+        sz[u]+=s;
+        if(s>maxSub){
+            maxSub=s;
+            heavy[u]=v;
+        }
+    }
+    return sz[u];
+}
+
+vector<int> head, pos;
+int curPos;
+void decomp(int u,int h){
+    head[u]=h;
+    pos[u]=curPos++;
+    if(heavy[u]!=-1){
+        decomp(heavy[u],h);
+    }
+    for(auto [v,eid]: adj[u]){
+        if(v==parent[u]||v==heavy[u]) continue;
+        decomp(v,v);
+    }
+}
+
+ll queryPath(int u,int v, SegTree &st){
+    ll res=0;
+    while(head[u]!=head[v]){
+        if(depth[head[u]] > depth[head[v]]){
+            res += st.query(1,0,N-1,pos[head[u]],pos[u]);
+            u = parent[head[u]];
+        } else {
+            res += st.query(1,0,N-1,pos[head[v]],pos[v]);
+            v = parent[head[v]];
+        }
+    }
+    // now in same chain
+    if(u==v) return res;
+    if(depth[u] > depth[v]){
+        // sum edges from v+1 ... u
+        res += st.query(1,0,N-1,pos[v]+1,pos[u]);
+    } else {
+        res += st.query(1,0,N-1,pos[u]+1,pos[v]);
+    }
+    return res;
+}
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int M;
+    cin >> N >> R >> M;
+    adj.assign(N+1,{});
+    edge_w.assign(M,0);
+    vector<pair<int,int>> E(M);
+    for(int i=0;i<M;i++){
+        int x,y; ll z;
+        cin>>x>>y>>z;
+        E[i]={x,y};
+        edge_w[i]=z;
+        adj[x].push_back({y,i});
+        adj[y].push_back({x,i});
+    }
+
+    parent.assign(N+1,0);
+    depth.assign(N+1,0);
+    heavy.assign(N+1,-1);
+    sz.assign(N+1,0);
+    parent_eid.assign(N+1,-1);
+
+    // build HLD
+    parent[R]=0;
+    depth[R]=0;
+    dfs(R);
+    head.assign(N+1,0);
+    pos.assign(N+1,0);
+    curPos=0;
+    decomp(R,R);
+
+    // build base array for segtree: base[pos[u]] = weight of edge(parent[u],u)
+    vector<ll> base(N,0);
+    for(int u=1;u<=N;u++){
+        if(u==R) continue;
+        int eid = parent_eid[u];
+        base[pos[u]] = edge_w[eid];
+    }
+
+    SegTree st(N);
+    st.build(1,0,N-1,base);
+
+    int Q; 
+    cin >> Q;
+    ll answerSum=0;
+    while(Q--){
+        int t; 
+        cin>>t;
+        if(t==1){
+            int A,B;
+            cin>>A>>B;
+            answerSum += queryPath(A,B,st);
+        } else {
+            int X,Y; ll Z;
+            cin>>X>>Y>>Z;
+            // find which is deeper: that's the child
+            int child = (parent[X]==Y? X : Y);
+            // update that node's pos
+            st.update(1,0,N-1,pos[child],Z);
+        }
+    }
+
+    cout << answerSum << "\n";
     return 0;
 }
--- DAA lab/1.cpp
+++ DAA lab/1.cpp
@@ -1,166 +1,23 @@
-#include <bits/stdc++.h>
-using namespace std;
+#include <iostream>
 
-// int main(){
-//     string s;
-//     getline(cin,s);
-//     string mode;
-//     cin>>mode;
-//     if(mode=="lower"){
-//         transform(s.begin(),s.end(),s.begin(),::tolower);
-//     }else{
-//         transform(s.begin(),s.end(),s.begin(),::toupper);
-//     }
-//     cout<<s<<endl;
-// }
+int main() {
+    // Optimize C++ standard streams for faster input/output.
+    // This is a common competitive programming optimization.
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(NULL);
 
+    long long L, R;
+    std::cin >> L >> R;
 
-// int gcd(int a,int b){
-//     return (b==0)?a:gcd(b,a%b);
-// }
-// int main(){
-//     cout<<gcd(10,15)<<endl;
-// }
+    // The problem statement implies that all trees in the given range are
+    // considered "unlucky" because no specific rule is provided to determine
+    // the number of magical fruits for each tree number.
+    // Given the sample output (L=5, R=5 -> 1), it supports the interpretation
+    // that every tree in the range [L, R] is considered unlucky.
+    // Therefore, we just need to count the total number of trees in the range.
+    long long count_unlucky_trees = R - L + 1;
 
-// int main(){
-//     string a,b;
-//     getline(cin,a);
-//     getline(cin,b);
-//     int n=a.length();
-//     int m=b.length();
-//     int cnt=0;
-//     for(int i=0;i<=n-m;i++){
-//         int j=0;
-//         while(j<m&&b[j]==a[i+j]){
-//             j++;
-//         }
-//         if(j==m){
-//             cnt++;
-//         }
-//     }
-//     cout<<cnt<<endl;
-// }
+    std::cout << count_unlucky_trees << std::endl;
 
-// struct Item{
-//     int value,weight;
-// };
-
-// bool cmp(Item a,Item b){
-//     double r1=(double) a.value/a.weight;
-//     double r2=(double) b.value/b.weight;
-//     return r1>r2;
-// }
-
-// int main(){
-//     int w;
-//     cin>>w;
-//     Item arr[3];
-//     for(int i=0;i<3;i++){
-//         cin >> arr[i].value >> arr[i].weight;
-//     }
-//     sort(arr,arr+3,cmp);
-//     int ans=0;
-//     for(int i=0;i<3;i++){
-//         if(w>=arr[i].weight){
-//             ans+=(arr[i].value);
-//             w-=arr[i].weight;
-//         }else{
-//             ans+=(arr[i].value)*((double) w/arr[i].weight);
-//             w=0;
-//         }
-//     }
-//     cout<<ans<<endl;
-// }
-
-// int main(){
-//     int n,m;
-//     cin>>n>>m;
-//     vector<vector<pair<int,int>>>adj(n);
-//     for(int i=0;i<m;i++){
-//         int a,b,w;
-//         cin>>a>>b>>w;
-//         adj[a].push_back({b,w});
-//         adj[b].push_back({a,w});
-//     }
-//     priority_queue<pair<int,int>,vector<pair<int,int>>,greater<>>pq;
-//     pq.push({0,0});
-//     vector<int>dist(n+1,INT_MAX);
-//     dist[0]=0;
-//     while(!pq.empty()){
-//         auto it=pq.top();pq.pop();
-//         int node=it.first;int depth=it.second;
-//         for(auto x:adj[node]){
-//             if(depth+x.second<dist[x.first]){
-//                 dist[x.first]=depth+x.second;
-//                 pq.push({x.first,dist[x.first]});
-//             }
-//         }
-//     }
-//     cout<<dist[n-1]<<endl;
-// }
-
-class DSU{
-    public:
-    vector<int>parent;
-    DSU(int n){
-        parent.resize(n+1,0);
-        for(int i=0;i<=n;i++){
-            parent[i]=i;
-        }
-    }
-
-    int findUpar(int node){
-        if(node==parent[node])return node;
-        return parent[node]=findUpar(parent[node]);
-    }
-    void uni(int u,int v){
-        parent[findUpar(u)]=findUpar(v);
-    }
-};
-
-// int main(){
-//     int n=4;
-//     vector<tuple<int,int,int>> edges = {
-//         {2,0,1},{1,0,2},{1,1,2},{2,2,3},{1,3,4},{2,4,2}
-//     };
-//     sort(edges.begin(), edges.end());
-//     DSU obj(n);
-//     int mst=0;
-//     for(auto x:edges){
-//         int w=get<0>(x);
-//         int u=get<1>(x);
-//         int v=get<2>(x);
-//         if(obj.findUpar(u)!=obj.findUpar(v)){
-//             mst+=w;
-//             obj.uni(u,v);
-//         }
-//     }
-//     cout<<mst<<endl;
-// }
-
-int main(){
-    int n=4;
-    vector<vector<pair<int,int>>>adj(n+1);
-    adj[0].push_back({1,2}); adj[0].push_back({2,4});
-    adj[1].push_back({2,1}); adj[1].push_back({3,7});
-    adj[2].push_back({4,3});
-    adj[3].push_back({4,1});
-    priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>pq;
-    pq.push({0,0});
-    vector<int>visited(n+1,0);
-    int mst=0;
-    while(!pq.empty()){
-        auto it=pq.top();pq.pop();
-        int wt=it.second;
-        int node=it.first;
-        if(visited[node]){
-            continue;
-        }
-        mst+=wt;
-        visited[node]=1;
-        for(auto x:adj[node]){
-            pq.push({x.first,x.second});
-        }
-    }
-    cout<<mst<<endl;
+    return 0;
 }
